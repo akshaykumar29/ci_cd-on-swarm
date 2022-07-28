d0d3916a712fe77cf83d92dac35813f29da9baf3
akshaykumar29 <102212425+akshaykumar29@users.noreply.github.com>
2022-07-28 16:19:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+import pandas as pd
+import redis
+import pymongo
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello from Python!"
 
-if _name_ == "__main__":
-    app.run(host='0.0.0.0')
+@app.route('/')
+def home():
+    return {'msg': 'working'}
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3089)
